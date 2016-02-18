889b9e0b082a11f6519679f3a93ce483efc09464
Anna Becker and Brad London <anna.r.becker@gmail.com>
2016-02-18 08:36:47 -0800

    Add passing test to getTask method.

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -2,13 +2,18 @@ import java.util.ArrayList;
 
 public class Category {
 
+  private static ArrayList<Category> instances = new ArrayList<Category>();
   private String mName;
+  private ArrayList<Task> mTasks;
 
   public Category(String name) {
     mName = name;
+    mTasks = new ArrayList<Task>();
   }
   public String getName() {
     return mName;
   }
-
+  public ArrayList<Task> getTasks() {
+    return mTasks;
+  }
 }
